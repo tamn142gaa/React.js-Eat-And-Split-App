35fc13a2bd7b4c6114dab506e43aa4bf8c566ed5
Youssouf Cherif Bichara <youssoufcherifbich@gmail.com>
2024-03-19 02:34:10 +0100

    Define handleSplitBil function

--- src/App.js
+++ src/App.js
@@ -40,6 +40,16 @@ export default function App() {
     setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
   }
 
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
@@ -53,7 +63,10 @@ export default function App() {
           {showAddFriend ? "Close" : "Add friend"}
         </Button>
       </div>
-      <FormSplitBill selectedFriend={selectedFriend} />
+      <FormSplitBill
+        selectedFriend={selectedFriend}
+        onSplitBill={handleSplitBill}
+      />
     </div>
   );
 }
